9dce3441dd23b13db32a7fe0f46a4ea628c8abea
Akash-Kolekar <akashkolekar2003@gmail.com>
2024-04-30 20:44:35 +0530

    update

---
+++ test/unit/MultiCall.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {MultiCall, TestMultiCall} from "../../src/MultiCall.sol";
+import {Test, console} from "forge-std/Test.sol";
+
+contract TestMultipleCall is Test {}
